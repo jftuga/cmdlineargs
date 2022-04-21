21869365429c831f2a6e61d915fe48127cf3e410
John Taylor <jftuga@users.noreply.github.com>
2022-04-21 07:29:25 -0400

    add feature: save to file

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # cmdlineargs
-Output all given command line arguments
+
+Output all given command line arguments to STDOUT unless the `CMDLINEARGS_FILE` environment variable is set.
+In that case, append the command line arguments to the file stored in this value.
 
 ## Example
 
 ```
 PS R:\> .\cmdlineargs.exe aaa "with spaces" bbb ccc 'another with spaces' ddd eee
 
-length: 8
-
-[ 0] R:\cmdlineargs.exe
+========================================================================
+2022-04-21T07:20:25-04:00
+[ 0] C:\Users\john\go\src\github.com\jftuga\cmdlineargs\cmdlineargs.exe
 [ 1] aaa
 [ 2] with spaces
 [ 3] bbb
--- main.go
+++ main.go
@@ -1,17 +1,44 @@
+/*
+main.go
+-John Taylor
+2021-05-13
+
+Output all given command line arguments to STDOUT unless the CMDLINEARGS_FILE environment variable is set.
+In that case, append the command line arguments to the file stored in this value.
+*/
+
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
-const version string = "1.0.0"
+const version string = "1.1.0"
 
 func main() {
-	fmt.Println()
-	fmt.Println("length:", len(os.Args))
-	fmt.Println()
-	for i, arg := range os.Args {
-		fmt.Printf("[%2d] %s\n", i, arg)
+	var f *os.File
+	var err error
+	saveToFile := os.Getenv("CMDLINEARGS_FILE")
+	if len(saveToFile) > 0 {
+		f, err = os.OpenFile(saveToFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening file: %s\n", saveToFile)
+			os.Exit(1)
+		}
+	} else {
+		f = os.Stdout
 	}
+	writer := bufio.NewWriter(f)
+	writer.WriteString("\n========================================================================\n")
+	dt := time.Now()
+	writer.WriteString(fmt.Sprintf("%s\n", dt.Format(time.RFC3339)))
+	//writer.WriteString(fmt.Sprintf("length: %d\n\n", len(os.Args)))
+	for i, arg := range os.Args {
+		writer.WriteString(fmt.Sprintf("[%2d] %s\n", i, arg))
+	}
+	writer.Flush()
+	f.Close()
 }
